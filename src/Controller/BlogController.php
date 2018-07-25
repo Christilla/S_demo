2581257396a39dddc225f57ac10eaf94ec893574
Christilla <cpuyjarinet@gmail.com>
2018-07-25 14:39:59 +0200

    Rajout de formulaire

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,8 +4,14 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+
 
 class BlogController extends Controller
 {
@@ -37,6 +43,54 @@ class BlogController extends Controller
     }
 
     /**
+     * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit", name="blog_edit")
+     */
+
+    public function form(Article $article = null, Request $request, ObjectManager $manager ){
+
+                    if (!$article) {
+            $article = new Article(); 
+
+                    }
+          
+
+        /*  $article->setTitle("Titre de l'article")
+               ->setContent("le contenu de l'article");  */
+
+
+        $form = $this->createFormBuilder($article)
+                     ->add('title')
+                     ->add('content')
+                     ->add('image')
+
+                     /* ->add('save', SubmitType::class, [
+                         'label' => 'Enregistrer'
+                     ]) */
+                     ->getForm();  
+
+        $form->handleRequest($request);
+        /* dump($article); */
+
+        if($form->isSubmitted()&& $form->isValid()) {
+            if(!$article->getId()){
+                $article->setCreateAt(new \DateTime());
+            }
+                
+                $manager->persist($article);
+                $manager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
+
+        return $this->render('blog/create.html.twig',[
+            'formArticle' => $form->createView(),
+            'editMode' => $article->getId() !== null
+        ]);
+    }
+
+
+    /**
      * @Route("/blog/{id}", name="blog_show")
      */
 
@@ -47,5 +101,7 @@ class BlogController extends Controller
             'article' => $article
         ]);
     }
+
+    
 }
  
